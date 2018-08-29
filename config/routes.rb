ae73b8617cf1b133ed20a8e1d1ad6e59d978b3b3
Kristine <kteich88@gmail.com>
2018-08-28 20:48:32 -0400

    added routes based off another sites useful routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
+
+  get  '/hark',     to: 'pages#hark'
+  get 'harks/:name', to: 'posts#index'
+
+  get  '/raven',    to: 'pages#about'
+  get  '/contact',  to: 'pages#contact'
+
+  get 'password_resets/new'
+  get 'password_resets/edit'
+
+  get    '/signup',  to: 'users#new'
+  post   '/signup',  to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
+  resources :users
+  resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
+  resources :posts, shallow: true do
+  	resources :comments, only: [:create, :destroy]
+  end
 end
